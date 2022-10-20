b805ffb8697acd573839bc12694a2dae6a0f9b6d
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-10-19 22:41:54 -0700

    migrated files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../config";
+
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container"></div>
+      <div className="container"></div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,2 @@
+const apiKey = "f4cfeb4644a805a74f5f147d17164b18";
+export default apiKey;
