75f5be901dce477a82421186ee8fdcb3ce60a32a
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-10-21 23:37:11 -0700

    photos are rendering

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -2,14 +2,14 @@ import React from "react";
 // import NotFound from "./NotFound";
 import Photos from "./Photos";
 
-// REFERENCE:  `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
 const photoContainer = ({ photoData }) => {
   let photoList = photoData.map((photo) => (
     <Photos
       server={photo.server}
-      key={photo.id}
+      id={photo.id}
       secret={photo.secret}
       title={photo.title}
+      key={photo.id}
     />
   ));
 
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+// REFERENCE:  `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
 const Photos = (props) => (
   <li>
     <img
-      src={`https://live.staticflickr.com/${props.server}/${props.key}_${props.secret}.jpg`}
+      src={`https://live.staticflickr.com/${props.server}/${props.id}_${props.secret}.jpg`}
       alt={props.title}
     />
   </li>
